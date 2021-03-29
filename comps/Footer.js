ed56d7e838eaa7012702f013909698b1a6028e33
Hakan Degirmen <hakandegirmen@live.com>
2021-03-29 16:09:11 -0400

    initial

---
+++ comps/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return (<footer><p>Copyright 2021 NInjaas</p></footer>);
+}
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+      <h2>about</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio porro non, earum tempore molestias cupiditate aliquam facere quam exercitationem ducimus! Illo molestias vel saepe. Consectetur velit quae dignissimos mollitia deleniti!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio porro non, earum tempore molestias cupiditate aliquam facere quam exercitationem ducimus! Illo molestias vel saepe. Consectetur velit quae dignissimos mollitia deleniti!</p>
+    </div>
+  );
+}
+
+export default About;
